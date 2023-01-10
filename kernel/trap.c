1064048121572835b2a468abbee52f8f1f5e6712
gongrunze <823650460@qq.com>
2023-01-10 19:50:57 +0800

    task2 Lazy allocation

--- kernel/trap.c
+++ kernel/trap.c
@@ -67,7 +67,18 @@ usertrap(void)
     syscall();
   } else if((which_dev = devintr()) != 0){
     // ok
-  } else {
+  } else if (r_scause() == 13 || r_scause() == 15)
+  {
+      // 分配物理内存
+      char *mem;
+      mem = kalloc();
+      memset(mem, 0, PGSIZE);
+      if(mappages(p->pagetable, PGROUNDDOWN(r_stval()), PGSIZE, (uint64)mem, PTE_W|PTE_X|PTE_R|PTE_U) != 0){
+          kfree(mem);
+          p->killed = 1;
+      }
+  }
+  else {
     printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
     printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
     p->killed = 1;
