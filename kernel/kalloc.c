8fae7e404554e8bfc903c64e96060641ad946518
gongrunze <823650460@qq.com>
2023-02-19 22:35:45 +0800

    完成 lab8.1

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -10,7 +10,7 @@
 #include "defs.h"
 
 void freerange(void *pa_start, void *pa_end);
-
+void mykfree(void *pa,int pro);
 extern char end[]; // first address after kernel.
                    // defined by kernel.ld.
 
@@ -18,27 +18,55 @@ struct run {
   struct run *next;
 };
 
-struct {
+//struct kmem{
+//    struct spinlock lock;
+//    struct run *freelist;
+//} kmem
+
+// lab8.1 为每个CPU维护一个空闲列表
+struct kmem{
   struct spinlock lock;
   struct run *freelist;
-} kmem;
+} kmem[NCPU];
 
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
-  freerange(end, (void*)PHYSTOP);
+    // lab8.1 为每个CPU分配一个锁
+    for (int i = 0; i < NCPU; ++i) {
+        initlock(&kmem[i].lock, "kmem");
+    }
+    freerange(end, (void*)PHYSTOP);
 }
 
 void
 freerange(void *pa_start, void *pa_end)
 {
-  char *p;
-  p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
-    kfree(p);
+    char *p;
+    p = (char*)PGROUNDUP((uint64)pa_start);
+    // freerange将所有可用内存分配给运行freerange的CPU。
+    for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
+        kfree(p);
 }
 
+void
+mykfree(void *pa,int pro)
+{
+    struct run *r;
+
+    if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
+        panic("kfree");
+
+    // Fill with junk to catch dangling refs.
+    memset(pa, 1, PGSIZE);
+
+    r = (struct run*)pa;
+
+    acquire(&kmem[pro].lock);
+    r->next = kmem[pro].freelist;
+    kmem[pro].freelist = r;
+    release(&kmem[pro].lock);
+}
 // Free the page of physical memory pointed at by v,
 // which normally should have been returned by a
 // call to kalloc().  (The exception is when
@@ -56,10 +84,17 @@ kfree(void *pa)
 
   r = (struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  // lab8.1 使用当前cpu的空闲列表和锁
+  push_off();
+  int id = cpuid();
+
+
+  acquire(&kmem[id].lock);
+  r->next = kmem[id].freelist;
+  kmem[id].freelist = r;
+  release(&kmem[id].lock);
+
+  pop_off(); // 只有在中断关闭时调用它并使用其结果才是安全的
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -70,11 +105,27 @@ kalloc(void)
 {
   struct run *r;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
+  // lab8.1 使用当前cpu的空闲列表和锁
+  push_off();
+  int id = cpuid();
+
+  acquire(&kmem[id].lock);
+  r = kmem[id].freelist;
   if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+    kmem[id].freelist = r->next;
+  else{ // lab8.1 当前CPU无可用内存时“窃取”另一个CPU空闲列表的一部分
+      for (int i = 0; i < NCPU; ++i) {
+          if (kmem[i].freelist)
+          {
+              r = kmem[i].freelist;
+              kmem[i].freelist = kmem[i].freelist->next;
+              break;
+          }
+      }
+  }
+  release(&kmem[id].lock);
+
+  pop_off();
 
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
